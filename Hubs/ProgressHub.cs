0cb8a3be943de2f82788837c23e932e152012709
Dale Tiley <dale@mroofing.co.za>
2025-09-06 16:33:02 +0200

    Add files via upload

---
+++ Hubs/ProgressHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace MillenniumWebFixed.Hubs
+{
+    public class ProgressHub : Hub
+    {
+        public static void Send(string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+            context.Clients.All.receiveProgress(message);
+        }
+    }
+}
---
+++ Hubs/QuoteProgressHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace MillenniumWebFixed.Hubs
+{
+    public class QuoteProgressHub : Hub
+    {
+        public static void Send(string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<QuoteProgressHub>();
+            context.Clients.All.receiveQuoteProgress(message);
+        }
+    }
+}
